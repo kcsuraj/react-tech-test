7d5d1ca8b95c9ec87a29b9ca121c84c805165bcf
Suraj kc <suraj.kc@outlook.com>
2022-07-08 08:36:09 +0545

    Use states object for select options & add comments

--- src/components/inputField/InputField.tsx
+++ src/components/inputField/InputField.tsx
@@ -4,11 +4,16 @@
 import { ChangeEvent, FC } from 'react';
 
 interface IProps {
+  /** Name for Select attribute */
   name: string;
+  /** Type of input element */
   type: 'text' | 'number';
+  /** Label content to display */
   label: string;
   placeholder: string;
+  /** Callback fired when a menu item is selected */
   onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  /** Value selected in select */
   value: string;
 }
 
